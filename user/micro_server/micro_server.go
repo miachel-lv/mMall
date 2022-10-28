054f9870860300aaee36ae995f5cfd189d00a081
lwx <lwx@lbx.dev>
2022-10-28 17:50:15 +0800

    更新micro server 入口

--- user/micro_server/micro_server.go
+++ user/micro_server/micro_server.go
@@ -1,1 +1,26 @@
 package micro_server
+
+import (
+	"github.com/asim/go-micro/v3"
+	"github.com/asim/go-micro/v3/logger"
+	"user/core"
+	"user/services"
+)
+
+func Init() {
+	srv :=  micro.NewService(
+		micro.Name("user"),
+		micro.Version("latest"),
+		//micro.Address(":8888"),
+	)
+	srv.Init()
+	// Register handler
+	if err := services.RegisterUserServiceHandler(srv.Server(), new(core.UserService)); err != nil {
+		logger.Fatal(err)
+	}
+
+	// Run service
+	if err := srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
