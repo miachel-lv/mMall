d4e954007537b3b8b87e73974c9ed8385281aa1e
lwx <lwx@lbx.dev>
2022-12-02 16:42:03 +0800

    添加辅助方法

--- user/pkg/e/code.go
+++ user/pkg/e/code.go
@@ -4,4 +4,10 @@ const (
 	SUCCESS       = 200
 	InvalidParams = 400
 	ERROR         = 500
+
+	//管理员错误
+	ErrorAuthCheckTokenFail        = 30001 //token 错误
+	ErrorAuthCheckTokenTimeout     = 30002 //token 过期
+	ErrorAuthToken                 = 30003
+	ErrorAuth                      = 30004
 )
